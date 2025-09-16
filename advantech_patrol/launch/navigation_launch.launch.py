4345ec7ffe344252bd0914c9ce27925c5f9e099f
Yakir <yakir@cogniteam.com>
2025-09-16 09:02:06 +0300

    mav2_params

--- advantech_patrol/launch/navigation_launch.launch.py
+++ advantech_patrol/launch/navigation_launch.launch.py
@@ -30,6 +30,9 @@ def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory('nav2_bringup')
 
+    nav_params_dir = get_package_share_directory('advantech_patrol')
+
+
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
@@ -85,7 +88,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        default_value=os.path.join(nav_params_dir, 'params', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
